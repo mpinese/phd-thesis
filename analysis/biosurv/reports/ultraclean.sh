9e62a29df652ff60fd565e6d57946d52e69a71d1
Mark Pinese <mark.pinese@gmail.com>
2014-11-23 23:59:59 +1100

    Finally fixed the Wolfpack issues.  Combined the various SIS-NMF knits into one master file called with varying parameters.  Started on meta-metagene work (15_SIS_NMF_combined) to reduce interpretation load.

--- analysis/biosurv/reports/ultraclean.sh
+++ analysis/biosurv/reports/ultraclean.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
-cd 09_SIS_diag_dsd/ && make ultraclean && cd ..
-cd 10_SIS_surg_dsd/ && make ultraclean && cd ..
-cd 11_SIS_recr_dsd/ && make ultraclean && cd ..
-cd 12_SIS_diag_rec/ && make ultraclean && cd ..
-cd 13_SIS_surg_rec/ && make ultraclean && cd ..
+cd 09_SIS_diag_dsd/ && make -f ../common/SIS_NMF_Makefile ultraclean && cd ..
+cd 10_SIS_surg_dsd/ && make -f ../common/SIS_NMF_Makefile ultraclean && cd ..
+cd 11_SIS_recr_dsd/ && make -f ../common/SIS_NMF_Makefile ultraclean && cd ..
+cd 12_SIS_diag_rec/ && make -f ../common/SIS_NMF_Makefile ultraclean && cd ..
+cd 13_SIS_surg_rec/ && make -f ../common/SIS_NMF_Makefile ultraclean && cd ..
