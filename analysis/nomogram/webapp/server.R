993959a0b76809dd9911ac54cb4a0b99b05551ff
Mark Pinese <mark.pinese@gmail.com>
2015-03-24 11:48:55 +0000

    nomogram: Tweaked figures for readability webapp: Updated to PCOP 1.0; this should be the final version.  Got a domain name for it as well.

--- analysis/nomogram/webapp/server.R
+++ analysis/nomogram/webapp/server.R
@@ -1,93 +1,14 @@
 library(shiny)
-library(randomForestSRC)
-library(survival)
+library(flexsurv)
 library(ggplot2)
-library(rms)
 
-load("../03_NSWPCN_subset.rda")
-data.all = cbind(time = data.y[,1], event = data.y[,2], data.x.all, data.x.extra)
-
-dd <<- datadist(data.all)
-options(datadist = "dd")
-
-rsf.molec_preop = rfsrc(
-	Surv(time, event) ~ 
-		Patient.Sex + 
-		History.Diagnosis.AgeAt.Cent + 
-		Path.LocationBody + 
-		Path.Size.Cent + 
-		Molec.S100A4.DCThresh + 
-		Molec.S100A2.DCThresh, 
-	data = data.all,
-	mtry = 1,
-	splitrule = "logrankscore",
-	nsplit = 2, 
-	ntree = 1000)
-
-km.molec_preop = survfit(
-	Surv(time, event) ~ 
-		Molec.S100A4.DCThresh + 
-		Molec.S100A2.DCThresh, 
-	data = data.all)
-km.molec_preop.strata = rep(gsub(" ", "", names(km.molec_preop$strata)), km.molec_preop$strata)
-
-# cox.molec_preop.null = cph(
-# 	Surv(time, event) ~ 1,
-# 	data = data.all, x = TRUE, y = TRUE)
-# par(ask = TRUE)
-# for (i in 1:ncol(data.all)) { scatter.smooth(data.all[,i], resid(cox.molec_preop.null, type = "martingale"), main = colnames(data.all)[i]); abline(h = 0) }
-# Looks like a threshold effect for size.  Introduce a new
-# variable to code this.
-data.all$Path.Size.Cent.Thresh = pmin(data.all$Path.Size.Cent, 0)
-dd <<- datadist(data.all)
-options(datadist = "dd")
-
-# zph analysis indicated violations for Patient.Sex and Molec.S100A4.DCThresh.
-# Stratify on these to get around the problem
-cox.molec_preop = cph(
-	Surv(time, event) ~ 
-		strat(Patient.Sex) + 
-		History.Diagnosis.AgeAt.Cent + 
-		Path.LocationBody + 
-		Path.Size.Cent.Thresh + 
-		strat(Molec.S100A4.DCThresh) + 
-		Molec.S100A2.DCThresh, 
-	data = data.all, x = TRUE, y = TRUE, surv = FALSE)
-# cox.zph(cox.molec_preop)			# All good now
-# par(ask = TRUE)
-# for (i in 1:ncol(data.all)) { scatter.smooth(data.all[,i], resid(cox.molec_preop, type = "deviance"), main = colnames(data.all)[i]); abline(h = 0) }
-
-weibull.molec_preop = psm(
-	Surv(time, event) ~ 
-		Patient.Sex + 
-		History.Diagnosis.AgeAt.Cent + 
-		Path.LocationBody + 
-		Path.Size.Cent + 
-		Molec.S100A4.DCThresh + 
-		Molec.S100A2.DCThresh, 
-	data = data.all, dist = "weibull", x = TRUE, y = TRUE)
-# anova(weibull.molec_preop)
-# fastbw(weibull.molec_preo)p
-# validate(weibull.molec_preop, B = 1000)
-
-
-# temp.start = 1
-# for (temp.i in 1:length(km.molec_preop$strata))
-# {
-# 	temp.times = km.molec_preop$time[temp.start:(temp.start+km.molec_preop$strata[temp.i])]
-# 	temp.surv = km.molec_preop$surv[temp.start:(temp.start+km.molec_preop$strata[temp.i])]
-# 	if (temp.i == 1) { 
-# 		plot(log(temp.times), log(-log(temp.surv)), type = "s", col = temp.i, xlab = "log(t)", ylab = "log(-log(hat(S)(t)))", ylim = c(-5, 2)) 
-# 	} else {
-# 		lines(log(temp.times), log(-log(temp.surv)), type = "s", col = temp.i) 
-# 	}
-# 	temp.start = temp.start + km.molec_preop$strata[temp.i] + 1
-# }
-# Not great fits to Weibull.  :/
+ENV.PCOP10 = new.env(parent = baseenv())
+load("../data/PCOP-1.0.rda", envir = ENV.PCOP10)
 
 
 library(RColorBrewer)
 pal = brewer.pal(3, "Dark2")
+
 
 
 calcSurvivalCurve = function(input, which)
@@ -95,43 +16,37 @@ calcSurvivalCurve = function(input, which)
 	if (which == 1)
 	{
 		newdata = data.frame(
-			Patient.Sex = factor(input$Patient.Sex1, levels = c("M", "F")), 
-			History.Diagnosis.AgeAt.Cent = input$History.Diagnosis.AgeAt1 - 68,
-			Path.LocationBody = !input$Path.LocationHead1, 
-			Path.Size.Cent = input$Path.Size1 - 30, 
-			Path.Size.Cent.Thresh = pmin(input$Path.Size1 - 30, 0),
-			Molec.S100A2.DCThresh = input$Molec.S100A2.DCThresh1, 
-			Molec.S100A4.DCThresh = input$Molec.S100A4.DCThresh1)
+			SexM = input$Patient.Sex1 == "M", 
+			LocBody = !input$Path.LocationHead1, 
+			SizeCent = input$Path.Size1 - 30, 
+			A2 = input$Molec.S100A2.DCThresh1, 
+			A4 = input$Molec.S100A4.DCThresh1)
 		prognostic = input$Prognostic1
+		ci = input$CIBands1
 	}
 	else
 	{
 		newdata = data.frame(
-			Patient.Sex = factor(input$Patient.Sex2, levels = c("M", "F")), 
-			History.Diagnosis.AgeAt.Cent = input$History.Diagnosis.AgeAt2 - 68,
-			Path.LocationBody = !input$Path.LocationHead2, 
-			Path.Size.Cent = input$Path.Size2 - 30, 
-			Path.Size.Cent.Thresh = pmin(input$Path.Size2 - 30, 0),
-			Molec.S100A2.DCThresh = input$Molec.S100A2.DCThresh2, 
-			Molec.S100A4.DCThresh = input$Molec.S100A4.DCThresh2)		
+			SexM = input$Patient.Sex2 == "M", 
+			LocBody = !input$Path.LocationHead2, 
+			SizeCent = input$Path.Size2 - 30, 
+			A2 = input$Molec.S100A2.DCThresh2, 
+			A4 = input$Molec.S100A4.DCThresh2)		
 		prognostic = input$Prognostic2
+		ci = input$CIBands2
 	}
 
-	if (prognostic == "rf")
+	if (prognostic == "pcop10")
 	{
-		temp = predict(rsf.molec_preop, newdata = newdata)
-		predobj = list(time = temp$time.interest, surv = temp$survival, lower = rep(NA, length(temp$time.interest)), upper = rep(NA, length(temp$time.interest)))
-	}
-	else if (prognostic %in% c("cox", "weibull"))
-	{
-		temp = survest(switch(prognostic, "cox" = cox.molec_preop, "weibull" = weibull.molec_preop), newdata = newdata, conf.int = 0.95)
-		predobj = temp
-	}
-	else if (prognostic == "km")
-	{
-		temp.stratum = sprintf("Molec.S100A4.DCThresh=%s,Molec.S100A2.DCThresh=%s", newdata$Molec.S100A4.DCThresh, newdata$Molec.S100A2.DCThresh)
-		temp.sel = km.molec_preop.strata == temp.stratum
-		predobj = list(time = km.molec_preop$time[temp.sel], surv = km.molec_preop$surv[temp.sel], lower = km.molec_preop$lower[temp.sel], upper = km.molec_preop$upper[temp.sel])
+		temp = summary(ENV.PCOP10$fit, newdata = newdata, ci = ci)[[1]]
+		if (ci)
+		{
+			predobj = list(time = temp$time, surv = temp$est, lower = temp$lcl, upper = temp$ucl)
+		}
+		else
+		{
+			predobj = list(time = temp$time, surv = temp$est, lower = rep(NA, nrow(temp)), upper = rep(NA, nrow(temp)))
+		}
 	}
 
 	predobj
@@ -146,8 +61,6 @@ alphaCol = function(col, alpha)
 
 plotSurvCI = function(times, lower, upper, col)
 {
-	# lines(times, lower, type = "s", lwd = 1, col = col, lty = "dotted")
-	# lines(times, upper, type = "s", lwd = 1, col = col, lty = "dotted")
 	x = c(times[1], rep(c(times[-1], rev(times)[-1]), each = 2))
 	y = c(rep(upper[-length(upper)], each = 2), rep(rev(lower)[-1], each = 2), upper[1])
 	polygon(x, y, col = col, border = NA)
@@ -187,3 +100,4 @@ shinyServer(function(input, output) {
 		}
   })
 })
+
--- analysis/nomogram/webapp/ui.R
+++ analysis/nomogram/webapp/ui.R
@@ -1,73 +1,49 @@
 library(shiny)
 
 shinyUI(fixedPage(
-	titlePanel("Pancreas Cancer Outcome Predictor"),
+	titlePanel("Pancreas Cancer Outcome Predictor v1.0"),
 
 	fixedRow(
 		column(3,
 			wellPanel(
 				h4("Curve 1"),
-				selectInput("Prognostic1", "Prognostic method:", 
-					list("Random Forest" = "rf",
-						"Cox" = "cox",
-						"Weibull" = "weibull",
-						"Kaplan-Meier" = "km")),
+				selectInput("Prognostic1", "Prognostic model:", 
+					list("PCOP 1.0" = "pcop10")),
 
-				conditionalPanel(
-					condition = 'input.Prognostic1 != "km"',
-					selectInput("Patient.Sex1", "Patient Sex:",
-								list("Male" = "M", "Female" = "F")),
+				selectInput("Patient.Sex1", "Patient Sex:",
+							list("Male" = "M", "Female" = "F")),
 
-					sliderInput("History.Diagnosis.AgeAt1", "Age at diagnosis:", 
-								min = 30, max = 90, value = 60),
+				sliderInput("Path.Size1", "Length of longest axis (mm):", 
+							min = 5, max = 120, value = 30),
 
-					sliderInput("Path.Size1", "Length of longest axis (mm):", 
-								min = 5, max = 120, value = 30),
-
-					checkboxInput("Path.LocationHead1", "Tumour in head of pancreas", TRUE)
-				),
+				checkboxInput("Path.LocationHead1", "Tumour in head of pancreas", TRUE),
 
 				checkboxInput("Molec.S100A2.DCThresh1", "S100A2 positivity", FALSE),
 				checkboxInput("Molec.S100A4.DCThresh1", "S100A4 positivity", TRUE),
 
-				conditionalPanel(
-					condition = 'input.Prognostic1 != "rf"',
-					checkboxInput("CIBands1", "Show confidence interval bands", FALSE)
-				)
+				checkboxInput("CIBands1", "Show confidence interval bands", FALSE)
 			),
 			wellPanel(
 				h4("Curve 2"),
 				checkboxInput("Show2", "Show curve", FALSE),
 
-				conditionalPanel(
-					selectInput("Prognostic2", "Prognostic method:", 
-						list("Random Forest" = "rf",
-							"Cox" = "cox",
-							"Weibull" = "weibull",
-							"Kaplan-Meier" = "km")),
-					condition = "input.Show2 == true",
+			 	conditionalPanel(
+			 		selectInput("Prognostic2", "Prognostic model:", 
+			 					list("PCOP 1.0" = "pcop10")),
+			 		condition = "input.Show2 == true",
 
-					conditionalPanel(
-						condition = 'input.Prognostic2 != "km"',
-						selectInput("Patient.Sex2", "Patient Sex:",
-									list("Male" = "M", "Female" = "F")),
+					selectInput("Patient.Sex2", "Patient Sex:",
+								list("Male" = "M", "Female" = "F")),
 
-						sliderInput("History.Diagnosis.AgeAt2", "Age at diagnosis:", 
-									min = 30, max = 90, value = 60),
+					sliderInput("Path.Size2", "Length of longest axis (mm):", 
+								min = 5, max = 120, value = 30),
 
-						sliderInput("Path.Size2", "Length of longest axis (mm):", 
-									min = 5, max = 120, value = 30),
-
-						checkboxInput("Path.LocationHead2", "Tumour in head of pancreas", TRUE)
-					),
+					checkboxInput("Path.LocationHead2", "Tumour in head of pancreas", TRUE),
 
 					checkboxInput("Molec.S100A2.DCThresh2", "S100A2 positivity", FALSE),
 					checkboxInput("Molec.S100A4.DCThresh2", "S100A4 positivity", TRUE),
 
-					conditionalPanel(
-						condition = 'input.Prognostic2 != "rf"',
-						checkboxInput("CIBands2", "Show confidence interval bands", FALSE)
-					)
+					checkboxInput("CIBands2", "Show confidence interval bands", FALSE)
 				)
 			)
 		),
@@ -76,9 +52,11 @@ shinyUI(fixedPage(
 			fluidRow(
 				plotOutput("survPlot")
 			)
-		),
-		column(4,
-			h4("Summary Statistics")
-		)
+		)#,
+		# column(4,
+		# 	h4("Summary Statistics"),
+		# 	"TODO"
+		# )
 	)
 ))
+
